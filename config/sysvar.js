2562c5753d424ddfcf46364750fb3f16481fa5ee
Léo Muniz <leomuniz.primos@gmail.com>
2020-02-14 16:53:51 -0300

    Update sysvar.js

--- config/sysvar.js
+++ config/sysvar.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     
-    jwtSecret:"sdf4JYq2VY2fF14SDt4DFt74NJ3aL8Acj",
+    jwtSecret:"******************************",
     devDB:"mongodb://localhost/vesi",
     prodDB:"mongodb://127.0.0.1:27017/vesi"
 
